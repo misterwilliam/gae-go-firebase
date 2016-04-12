0da277c3fad3116755c427ebb56b207bddd4333d
misterwilliam <mr.williamchang@gmail.com>
2016-04-12 13:41:45 -0700

    Stuff

--- hello.go
+++ hello.go
@@ -1,12 +1,37 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+  "bufio"
+  "fmt"
+  "net/http"
 )
 
 func init() {
     http.HandleFunc("/", handler)
+    firebaseSubscribe()
+}
+
+func firebaseSubscribe() {
+  req, err := http.NewRequest("GET",
+                              "https://gae-node-firebase.firebaseio.com/temp.json",
+                              nil)
+  req.Header.Add("Accept", "text/event-stream")
+  client := &http.Client{}
+  resp, err := client.Do(req)
+
+  if err != nil {
+    //fmt.Println("Got an error!!!!")
+    fmt.Printf("Error: %s", err)
+  }
+
+  reader := bufio.NewReader(resp.Body)
+  for {
+    line, err := reader.ReadBytes('\n')
+    if err != nil {
+      fmt.Println("Got an error!")
+    }
+    fmt.Printf("%s\n", line)
+  }
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
